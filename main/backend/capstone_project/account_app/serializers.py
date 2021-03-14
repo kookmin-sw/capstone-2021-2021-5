434a20d813052344e143f4b409cea181f8bbae8f
Jung-woo Lee <you@example.com>
2021-03-14 17:37:33 +0900

    회원가입 필드 추가

---
+++ main/backend/capstone_project/account_app/serializers.py
@@ -0,0 +1,18 @@
+from django.contrib.auth import get_user_model
+from rest_auth.registration.serializers import RegisterSerializer
+from rest_framework import serializers
+
+User = get_user_model()
+
+class CustomRegisterSerializer(RegisterSerializer):
+    """
+    회원가입을 위한 serializer
+    """
+    # 성별
+    gender = serializers.ChoiceField(choices=User.genderChoices)
+    birthDate = serializers.DateTimeField()
+    def get_cleaned_data(self):
+        data_dict = super().get_cleaned_data()
+        data_dict['gender'] = self.validated_data.get('gender', '')
+        data_dict['birthDate'] = self.validated_data.get('birthDate', '')
+        return data_dict
---
+++ main/backend/capstone_project/my_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',    
+        'NAME': 'alpha',                  
+        'USER': 'root',                        
+        'PASSWORD': 'hjkhjk66',                  
+        'HOST': 'localhost',                   
+        'PORT': '3306',                          
+    }
+}
