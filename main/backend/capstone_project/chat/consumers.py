92d24afbf5e557531cb4ff95c1e3b07fc6bf021d
Jung-woo Lee <you@example.com>
2021-05-02 18:19:07 +0900

    chatting

--- main/backend/capstone_project/chat/consumers.py
+++ main/backend/capstone_project/chat/consumers.py
@@ -7,6 +7,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
+        print( self.room_name)
         self.room_group_name = 'chat_%s' % self.room_name
         
         # Join room group
