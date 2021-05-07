9ef57227a35bf5b59e1561bfa328b95e4290fa46
Jung-woo Lee <you@example.com>
2021-05-07 18:29:52 +0900

    diary edition

--- main/backend/capstone_project/diary_app/serializers.py
+++ main/backend/capstone_project/diary_app/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from .models import Diary
 from django.core.exceptions import ValidationError
+import datetime
 
 
 class DiarySerializer(serializers.ModelSerializer):
@@ -16,6 +17,10 @@ class DiarySerializer(serializers.ModelSerializer):
     def validate(self, data):
 
         if Diary.objects.filter(profile=self.context['request'].user, title=data['title']).exists() == True: # 만약 같은 계정의 project title이 중복되면
-            raise ValidationError('중복된 title입니다')
+            raise ValidationError('duplicated title')
+        today = datetime.date.today()
+        if Diary.objects.filter(profile=self.context['request'].user, pubdate = today ).exists():
+            raise ValidationError('already written')
+        data['pubdate'] =  today
         data['profile'] = self.context['request'].user #project 생성시 항상 author을 해당 계정으로 설정
         return data
