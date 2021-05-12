90a68066ccd27ac7cdc65ea06ecd2b73ed8bde4e
Jung-woo Lee <you@example.com>
2021-05-13 00:36:33 +0900

    diary weather

--- main/backend/capstone_project/diary_app/serializers.py
+++ main/backend/capstone_project/diary_app/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from .models import Diary
 from django.core.exceptions import ValidationError
 import datetime
-
+from analysis.models import Emotion
 
 class DiarySerializer(serializers.ModelSerializer):
     """
@@ -21,6 +21,10 @@ class DiarySerializer(serializers.ModelSerializer):
         today = datetime.date.today()
         if self.context['request'].method != "PUT" and Diary.objects.filter(profile=self.context['request'].user, pubdate = today ).exists():
             raise ValidationError('already written')
+        if self.context['request'].method == "POST" and not Emotion.objects.filter(profile=self.context['request'].user,pubdate = today).exists():
+            raise ValidationError('not analyzed')
+
         data['pubdate'] =  today
+        data['weather'] =  Emotion.objects.get(profile=self.context['request'].user,pubdate = today).weather
         data['profile'] = self.context['request'].user #project 생성시 항상 author을 해당 계정으로 설정
         return data
