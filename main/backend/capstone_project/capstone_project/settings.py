a7a32433089fbb512006b9aad3ba331502ab2364
Jung-woo Lee <you@example.com>
2021-05-20 20:55:01 +0900

    deploy1

--- main/backend/capstone_project/capstone_project/settings.py
+++ main/backend/capstone_project/capstone_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@#!tz#jj#(fr1*m=ow=d+_ujlhb8w3tg5$#zn0@aol)2f+ak6k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
