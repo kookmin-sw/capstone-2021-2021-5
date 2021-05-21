63dc0cf46e162a2c5ca5c2bb2956abcde4b7d60e
Jung-woo Lee <you@example.com>
2021-05-21 17:10:25 +0900

    deploy2

--- main/backend/capstone_project/capstone_project/settings.py
+++ main/backend/capstone_project/capstone_project/settings.py
@@ -206,7 +206,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [('15.165.85.247', 6379)],
         },
     },
 }
