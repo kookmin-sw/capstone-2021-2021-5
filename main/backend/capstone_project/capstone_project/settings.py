1e152510d2a3fa103a8c7dbd4005567f7ec00783
Jung-woo Lee <you@example.com>
2021-05-28 01:16:31 +0900

    heelo

--- main/backend/capstone_project/capstone_project/settings.py
+++ main/backend/capstone_project/capstone_project/settings.py
@@ -215,6 +215,6 @@ CHANNEL_LAYERS = {
 }
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
